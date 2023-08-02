be0abb8d7b2b9c4fe8ad2d998065cc92ce3c17c2
Senyo Fiawornu <contactsenyo@gmail.com>
2023-08-02 19:54:18 +0000

    first commit

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,19 +4,19 @@
  * binary_tree_node - create a binary tree node
  * @parent: parent of node to create
  * @value: value to insert in the binary tree
- * Return: pointer to new node or NULL if failure
+ * Return: pointer to n_node node or NULL if failure
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new;
+	binary_tree_t *n_node;
 
-	new = malloc(sizeof(binary_tree_t));
-	if (!new)
+	n_node = malloc(sizeof(binary_tree_t));
+	if (!n_node)
 		return (NULL);
 
-	new->ne = value;
-	new->left = NULL;
-	new->right = NULL;
-	new->parent = parent;
-	return (new);
+	n_node->n = value;
+	n_node->left = NULL;
+	n_node->right = NULL;
+	n_node->parent = parent;
+	return (n_node);
 }
